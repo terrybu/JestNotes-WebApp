a047287028758a9ff8e7dfbebbed6576b27e9a20
Terry Bu <burea1124@gmail.com>
2014-12-29 12:08:14 -0500

    before changing things to username instead of just email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ParseUser
 	alias :email :username
-	validates :email, :email => true
-
+	validates_presence_of :email
 	validates_confirmation_of :password
 	validates_presence_of :password 
 	validates_length_of :password, minimum: 4
